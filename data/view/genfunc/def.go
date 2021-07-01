4f0b53aa45a9fd3df1d64d691cbe903ba06ba800
谢正才 <605586251@qq.com>
2021-07-01 16:51:16 +0800

    BaseMgr 改为可导出格式

--- data/view/genfunc/def.go
+++ data/view/genfunc/def.go
@@ -28,7 +28,7 @@ import (
 var globalIsRelated bool = true  // 全局预加载
 
 // prepare for other
-type _BaseMgr struct {
+type BaseMgr struct {
 	*gorm.DB
 	ctx       context.Context
 	cancel    context.CancelFunc
@@ -37,50 +37,50 @@ type _BaseMgr struct {
 }
 
 // SetCtx set context
-func (obj *_BaseMgr) SetTimeOut(timeout time.Duration) {
+func (obj *BaseMgr) SetTimeOut(timeout time.Duration) {
 	obj.ctx, obj.cancel = context.WithTimeout(context.Background(), timeout)
 	obj.timeout = timeout
 }
 
 // SetCtx set context
-func (obj *_BaseMgr) SetCtx(c context.Context) {
+func (obj *BaseMgr) SetCtx(c context.Context) {
 	if c != nil {
 		obj.ctx = c
 	}
 }
 
 // Ctx get context
-func (obj *_BaseMgr) GetCtx() context.Context {
+func (obj *BaseMgr) GetCtx() context.Context {
 	return obj.ctx
 }
 
 // Cancel cancel context
-func (obj *_BaseMgr) Cancel(c context.Context) {
+func (obj *BaseMgr) Cancel(c context.Context) {
 	obj.cancel()
 }
 
 // GetDB get gorm.DB info
-func (obj *_BaseMgr) GetDB() *gorm.DB {
+func (obj *BaseMgr) GetDB() *gorm.DB {
 	return obj.DB
 }
 
 // UpdateDB update gorm.DB info
-func (obj *_BaseMgr) UpdateDB(db *gorm.DB) {
+func (obj *BaseMgr) UpdateDB(db *gorm.DB) {
 	obj.DB = db
 }
 
 // GetIsRelated Query foreign key Association.获取是否查询外键关联(gorm.Related)
-func (obj *_BaseMgr) GetIsRelated() bool {
+func (obj *BaseMgr) GetIsRelated() bool {
 	return obj.isRelated
 }
 
 // SetIsRelated Query foreign key Association.设置是否查询外键关联(gorm.Related)
-func (obj *_BaseMgr) SetIsRelated(b bool) {
+func (obj *BaseMgr) SetIsRelated(b bool) {
 	obj.isRelated = b
 }
 
 // New new gorm.新gorm
-func (obj *_BaseMgr) New() *gorm.DB {
+func (obj *BaseMgr) New() *gorm.DB {
 	return obj.DB.Session(&gorm.Session{ Context: obj.ctx})
 }
 
